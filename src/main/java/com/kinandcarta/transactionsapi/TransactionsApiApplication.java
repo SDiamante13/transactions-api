ea3aac2e4ba555198e0d22fbdeba364e9d51aa24
Steven Diamante <stevendiamante13@gmail.com>
2022-02-03 16:18:16 -0500

    Initial commit.

---
+++ src/main/java/com/kinandcarta/transactionsapi/TransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kinandcarta.transactionsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsApiApplication.class, args);
+    }
+
+}
