6712f86b494f89b8c205d8f0acfca7e796693da5
Steven Diamante <stevendiamante13@gmail.com>
2022-02-08 14:38:14 -0500

    Add tearDown method to reset database.

--- src/test/java/com/kinandcarta/transactionsapi/TransactionsAcceptanceTest.java
+++ src/test/java/com/kinandcarta/transactionsapi/TransactionsAcceptanceTest.java
@@ -2,6 +2,7 @@ package com.kinandcarta.transactionsapi;
 
 import com.kinandcarta.transactionsapi.domain.Account;
 import com.kinandcarta.transactionsapi.repository.AccountRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,11 @@ class TransactionsAcceptanceTest {
     void setUp() {
         Account account = new Account(123L, "Bruce Wayne", emptySet());
         accountRepository.save(account);
+    }
+
+    @AfterEach
+    void tearDown() {
+        accountRepository.deleteAll();
     }
 
     /***
