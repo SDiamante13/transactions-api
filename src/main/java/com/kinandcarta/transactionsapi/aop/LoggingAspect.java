ead14b6b54f926f208cfe11d8ad6754ef31c9ac7
Steven Diamante <stevendiamante13@gmail.com>
2022-03-29 00:13:45 -0400

    Add an AOP logger that records the time taken to complete a transaction with the database.

---
+++ src/main/java/com/kinandcarta/transactionsapi/aop/LoggingAspect.java
@@ -0,0 +1,42 @@
+package com.kinandcarta.transactionsapi.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    public static final String EXECUTION_REPOSITORY = "execution(* com.kinandcarta.transactionsapi.repository..*(..)))";
+
+    @Around(EXECUTION_REPOSITORY)
+    public Object logMethodExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        final StopWatch stopWatch = new StopWatch();
+
+        Object result = callMethod(proceedingJoinPoint, stopWatch);
+        logTimeTaken(proceedingJoinPoint, stopWatch);
+
+        return result;
+    }
+
+    private Object callMethod(ProceedingJoinPoint proceedingJoinPoint, StopWatch stopWatch) throws Throwable {
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+        return result;
+    }
+
+    private void logTimeTaken(ProceedingJoinPoint proceedingJoinPoint, StopWatch stopWatch) {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        log.info("Execution time of " + stopWatch.getTotalTimeMillis() + " ms for " +
+                methodSignature.getDeclaringType().getSimpleName() +
+                "::" + methodSignature.getName()
+        );
+    }
+}
